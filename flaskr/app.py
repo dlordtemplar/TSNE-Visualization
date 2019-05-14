bec6ff82bd86cfa4e60ef6493df00b45647f1829
Pomi Park <dlordtemplar@gmail.com>
2019-05-14 12:21:38 +0200

    Remove unused setup from app.

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,44 +1,22 @@
 from flask import Flask
-import os
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+from . import common
+app.register_blueprint(common.bp)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+from . import neuron
+app.register_blueprint(neuron.bp)
 
-    from . import db
-    db.init_app(app)
+from . import pair
+app.register_blueprint(pair.bp)
 
-    from . import common
-    app.register_blueprint(common.bp)
-
-    from . import neuron
-    app.register_blueprint(neuron.bp)
-
-    from . import pair
-    app.register_blueprint(pair.bp)
-
-    from . import passage
-    app.register_blueprint(passage.bp)
-
-    return app
+from . import passage
+app.register_blueprint(passage.bp)
 
 if __name__ == '__main__':
-    app = create_app()
     app.run()
