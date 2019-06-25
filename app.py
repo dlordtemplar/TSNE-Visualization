7f1af9bf5747840a7b4d3ab87a8611c470eca199
pomi <dlordtemplar@gmail.com>
2019-06-25 15:46:28 +0200

    Add question viz.

--- app.py
+++ app.py
@@ -1,25 +1,20 @@
 from flask import Flask
 
-# create and configure the app
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_mapping(
-    SECRET_KEY='dev',
-)
-
 import common
-app.register_blueprint(common.bp)
-
 import neuron
-
-app.register_blueprint(neuron.bp)
-
 import pair
-
-app.register_blueprint(pair.bp)
-
 import passage
+import questions
 
+# create and configure the app
+app = Flask(__name__)
+app.config.from_pyfile('config.cfg', silent=False)
+
+app.register_blueprint(common.bp)
+app.register_blueprint(neuron.bp)
+app.register_blueprint(pair.bp)
 app.register_blueprint(passage.bp)
+app.register_blueprint(questions.bp)
 
 if __name__ == '__main__':
     app.run()
