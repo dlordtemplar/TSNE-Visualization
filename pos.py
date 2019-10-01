199a9d72b5a1d52767c5d2765f049c7b2a849bdc
dlordtemplar <dlordtemplar@gmail.com>
2019-09-30 21:51:38 -0700

    Update POS pages including navbar.

--- pos.py
+++ pos.py
@@ -13,42 +13,31 @@ DEFAULT_PERPLEXITY = 5
 TOTAL_PAIRS = 1790
 
 
-# class PairForm(Form):
-#     choices_pair = [(i, i) for i in range(TOTAL_PAIRS)]
-#     pair_num = SelectField('Pair', coerce=int, choices=choices_pair, default=0)
-#     choices_with_none = [(i, i) for i in range(NEURON_TOTAL)]
-#     choices_with_none.insert(0, (-1, 'None'))
-#     ca_neuron = SelectField('Neuron #', coerce=int, choices=choices_with_none, default=-1)
-#     wa_neuron = SelectField('Neuron #', coerce=int, choices=choices_with_none, default=-1)
-#     perplexity = IntegerField('Perplexity', validators=[validators.NumberRange(min=0)],
-#                               default=DEFAULT_PERPLEXITY)
-#     scale = BooleanField('Scale neurons')
-#     submit = SubmitField('Submit')
-
-
-@bp.route('/pos', strict_slashes=False, methods=['GET', 'POST'])
-def pair():
-    # if request.method == 'POST' and request.form:
-    #     form = PairForm(request.form)
-    #     if not form.validate():
-    #         return render_template('pair.html', form=form)
-    # else:
-    #     form = PairForm()
-    #     form.process()
-
-    url = current_app.config['REST_KERAS_URL'] + 'pos/'
+@bp.route('/pos/ptb', strict_slashes=False, methods=['GET', 'POST'])
+def pos_ptb():
+    url = current_app.config['REST_KERAS_URL'] + 'pos/ptb'
     headers = {'content-type': 'application/json; charset=utf-8'}
-    data = {
-        # 'pair_num': form.pair_num.data,
-        # 'perplexity': form.perplexity.data,
-        # 'ca_neuron': form.ca_neuron.data,
-        # 'wa_neuron': form.wa_neuron.data,
-        # 'scale': form.scale.data
-    }
-    response = requests.post(url, headers=headers) #, data=json.dumps(data))
+    response = requests.post(url, headers=headers)
     result = response.json()
-    print(result)
+    intro = 'Proof of concept created using Penn Treebank tags. For each question-answer pair, the part-of-speech tag with the highest amount of attention was recorded. Only the tags with the highest attention score were recorded and the others discarded. This was repeated for each neuron.'
+    return render_template('pos.html', plotly_pos=result, intro=intro)
 
-    return render_template('pos.html',
-                           plotly_pos=result
-                           )
+
+@bp.route('/pos/upos', strict_slashes=False, methods=['GET', 'POST'])
+def pos_upos():
+    url = current_app.config['REST_KERAS_URL'] + 'pos/upos'
+    headers = {'content-type': 'application/json; charset=utf-8'}
+    response = requests.post(url, headers=headers)
+    result = response.json()
+    intro = 'For each question-answer pair, each properly aligned universal dependencies universal part-of-speech tag\'s (UPOS) attention score was recorded. The result shows an overall percentage out of 100 of which tags a neuron paid attention to.'
+    return render_template('pos.html', plotly_pos=result, intro=intro)
+
+
+@bp.route('/pos/xpos', strict_slashes=False, methods=['GET', 'POST'])
+def pos_xpos():
+    url = current_app.config['REST_KERAS_URL'] + 'pos/xpos'
+    headers = {'content-type': 'application/json; charset=utf-8'}
+    response = requests.post(url, headers=headers)
+    result = response.json()
+    intro = 'For each question-answer pair, each properly aligned universal dependencies language-specific part-of-speech tag\'s (XPOS) attention score was recorded. The result shows an overall percentage out of 100 of which tags a neuron paid attention to.'
+    return render_template('pos.html', plotly_pos=result, intro=intro)
